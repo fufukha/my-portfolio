b5f4a16db93410cab93cd9e4bad3cd715642dc21
fufukha <wafakhalid29@gmail.com>
2021-04-14 11:44:57 -0400

    add Layout component to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from '@material-ui/styles'
+import Layout from '../components/Layout'
+import theme from '../theme'
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
