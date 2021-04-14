9c5627d9094733455537c7053fc3c06b7fe6921a
fufukha <wafakhalid29@gmail.com>
2021-04-14 11:42:58 -0400

    add Nav component

---
+++ components/Nav.tsx
@@ -0,0 +1,13 @@
+import { BottomNavigation, Button } from "@material-ui/core"
+
+const Nav = () => {
+  return (
+    <BottomNavigation component='nav'>
+      <Button variant='contained' color='primary'>
+        [download resume]
+      </Button>
+    </BottomNavigation>
+  )
+}
+
+export default Nav
