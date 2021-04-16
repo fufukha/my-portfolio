61c87a34faaee749c7b8012f911b2d0dfdf613ea
fufukha <wafakhalid29@gmail.com>
2021-04-16 14:55:49 -0400

    fix key warning

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,14 +1,16 @@
 import { Menu as MuiMenu } from '@material-ui/core'
 import Fade from '@material-ui/core/Fade'
 import IconButton from '@material-ui/core/IconButton'
+import MenuItem from '@material-ui/core/MenuItem'
+import Typography from '@material-ui/core/Typography'
 import MenuIcon from '@material-ui/icons/Menu'
 import { useRouter } from 'next/dist/client/router'
 import React from 'react'
-import MenuItems from './MenuItems'
 
 const Menu = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const router = useRouter()
+  const pages = ['home', 'projects']
 
   const handleOpen = (e: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(e.currentTarget)
@@ -20,6 +22,30 @@ const Menu = () => {
       router.push(path)
     }
   }
+
+  const items = ['[', ...pages, ']']
+
+  const menuItems = items.map((item, i) => {
+    let path: string
+    let onClick: React.MouseEventHandler | undefined
+    let disabled = true
+    let color: 'primary' | 'textPrimary' = 'textPrimary'
+
+    if (i !== 0 || i !== pages.length - 1) {
+      path = item === 'home' ? '/' : `/${item}`
+      onClick = () => handleClose(path)
+      disabled = false
+      color = router.pathname === path ? 'primary' : 'textPrimary'
+    }
+
+    return (
+      <MenuItem key={i} disabled={disabled} onClick={onClick}>
+        <Typography variant='h2' color={color}>
+          {item}
+        </Typography>
+      </MenuItem>
+    )
+  })
 
   return (
     <div>
@@ -43,7 +69,7 @@ const Menu = () => {
         onClose={() => handleClose()}
         onClick={() => handleClose()}
       >
-        <MenuItems handleClose={handleClose} />
+        {menuItems}
       </MuiMenu>
     </div>
   )
