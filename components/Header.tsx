bb63a3ed54f4f7e41494d5eea93cfa4488fff935
fufukha <wafakhalid29@gmail.com>
2021-04-17 12:31:36 -0400

    header elevation on scroll & scroll to top with header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,49 @@
+import { Theme } from '@material-ui/core'
 import AppBar from '@material-ui/core/AppBar'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import Toolbar from '@material-ui/core/Toolbar'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import makeStyles from '@material-ui/styles/makeStyles'
+import React from 'react'
 import Menu from './Menu'
 
+interface ElevationScrollProps {
+  children: React.ReactElement
+}
+
+function ElevationScroll({ children }: ElevationScrollProps) {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  })
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 10 : 0,
+  })
+}
+
+const useStyles = makeStyles<Theme>(({ palette }) => ({
+  appBar: {
+    background: palette.background.default,
+  },
+}))
+
 const Header = () => {
+  const classes = useStyles()
   return (
-    <AppBar position='fixed' color='transparent' elevation={0}>
-      <Toolbar component='header'>
-        <Menu />
-      </Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar
+        className={classes.appBar}
+        component='header'
+        position='fixed'
+        elevation={0}
+      >
+        <CssBaseline />
+        <Toolbar>
+          <Menu />
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
   )
 }
 
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,65 @@
-import { Container, makeStyles } from '@material-ui/core'
+import Container from '@material-ui/core/Container'
+import Fab from '@material-ui/core/Fab'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Zoom from '@material-ui/core/Zoom'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+import React from 'react'
 import Header from './Header'
 import Meta from './Meta'
 
+type ScrollToTopProps = {
+  children: React.ReactElement
+}
+
+const useStyles2 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  })
+)
+
+function ScrollTop(props: ScrollToTopProps) {
+  const { children } = props
+  const classes = useStyles2()
+  
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  })
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector('#back-to-top-anchor')
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' })
+    }
+  }
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role='presentation' className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  )
+}
 type LayoutProps = {
   children: JSX.Element
 }
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(({ mixins }) => ({
   container: {
-    padding: '96px 35px 70px 35px',
-    height: '100vh',
+    padding: '35px 35px 70px 35px',
+    height: `calc(100vh - ${mixins.toolbar.minHeight}px)`,
   },
-})
+}))
 
 const Layout = ({ children }: LayoutProps) => {
   const classes = useStyles()
@@ -20,7 +68,15 @@ const Layout = ({ children }: LayoutProps) => {
     <>
       <Meta />
       <Header />
-      <Container className={classes.container} component='main'>{children}</Container>
+      <Toolbar id='back-to-top-anchor' disableGutters />
+      <Container className={classes.container} component='main'>
+        {children}
+      </Container>
+      <ScrollTop>
+        <Fab color='primary' size='small' aria-label='scroll back to top'>
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
     </>
   )
 }
