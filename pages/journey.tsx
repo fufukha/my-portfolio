f192240645755e0ac3429cf13522f6168bbbfe31
fufukha <wafakhalid29@gmail.com>
2021-04-21 08:37:29 -0400

    added meta data and heading

--- pages/journey.tsx
+++ pages/journey.tsx
@@ -1,12 +1,29 @@
 import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 import VerticalStepper from '../components/VerticalStepper'
 import { steps } from '../utilis/projectsdata'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import Meta from '../components/Meta'
+
+const useStyles = makeStyles({
+  heading: {
+    marginBottom: '16px',
+  },
+})
 
 const journey = () => {
+  const classes = useStyles()
+
   return (
-    <Paper elevation={0}>
-      <VerticalStepper steps={steps} />
-    </Paper>
+    <>
+      <Meta title={'My Journey'} />
+      <Typography className={classes.heading} variant='h1' component='h1'>
+        My Journey
+      </Typography>
+      <Paper elevation={0}>
+        <VerticalStepper steps={steps} />
+      </Paper>
+    </>
   )
 }
 
