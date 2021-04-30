0fbcd506d4f299253e18dee2aab835a78081198f
fufukha <wafakhalid29@gmail.com>
2021-04-30 15:17:43 -0400

    edit step text

--- utilis/projectsdata.tsx
+++ utilis/projectsdata.tsx
@@ -46,9 +46,9 @@ export const steps: StepData[] = [
     content: (
       <>
         <span>Ruby</span> was the first language I learned. Its English-like
-        syntax is a great start for novices. I had a lot of fun building games in this
-        language; however, I wanted to create applications that ran in the
-        browser. Onward we go!
+        syntax is a great start for novices. I had a lot of fun building games
+        in this language; however, I wanted to create applications that ran in
+        the browser. Onward we go!
       </>
     ),
     url: '',
@@ -73,7 +73,7 @@ export const steps: StepData[] = [
       <>
         After learning the fundamentals of <span>responsive design</span> and
         design strategies such as <span>mobile-first</span>, I created sample
-        pages of my own design and clones.
+        pages of my own design, and clones.
       </>
     ),
     url: 'https://github.com/fufukha/minesweeper',
@@ -84,7 +84,7 @@ export const steps: StepData[] = [
     content: (
       <>
         Ah... <span>React</span>. Around this time, my most challenging{' '}
-        <span>React</span> applications was Minesweeper. Originally, I built it
+        <span>React</span> application was Minesweeper. Originally, I built it
         to focus on using <span>Redux</span> to manage the state but it also led
         me to delve deeper into <span>data structures and algorithms</span>.
         Since then I have built numerous <span>React</span> applications.
@@ -94,19 +94,20 @@ export const steps: StepData[] = [
     homepageUrl: 'https://fufukha.github.io/minesweeper/',
   },
   {
-    label: 'XStream Collaboration',
+    label: 'First Team Project',
     content: (
       <>
         I was invited to <span>collaborate</span> on a project. It was an
-        incredible experience to work alongside another developer. I was in
-        charge of designing and implementing certain features and paired with
-        the lead developer on many of the rest. I also presented my ideas that I
-        thought were meaningful to the project or that particular roll-out. This
-        was also my first time working with <span>Firebase</span>.
+        incredible experience to work alongside another developer. I was
+        responsible for designing and implementing entire features and paired
+        with the lead developer on several others. I also presented my ideas
+        that I thought were meaningful to the project or that particular
+        roll-out. This was also my first time working with <span>Firebase</span>
+        .
       </>
     ),
     url: 'https://github.com/abogalambo/xstream',
-    homepageUrl: 'http://x-stream-45773.firebaseapp.com/'
+    homepageUrl: 'http://x-stream-45773.firebaseapp.com/',
   },
   {
     label: 'TypeScript',
@@ -128,11 +129,11 @@ export const steps: StepData[] = [
     content: (
       <>
         I have gained a lot of experience building applications, and researching
-        and learning technologies to suit my needs. However, building and designing
-        web app as a just hobby is no longer scratching the itch. I am excited to
-        turn <span>front-end developer</span> into a full time{' '}
-        <span>career</span>. As for my study goals, I would love to explore
-        the <span>back-end</span> more and become more <span>Full Stack</span>.
+        and learning technologies to suit my needs. However, building and
+        designing web app as just a hobby is no longer scratching the itch. I am
+        excited to turn <span>front-end development</span> into a full time{' '}
+        <span>career</span>. As for my study goals, I would love to explore the{' '}
+        <span>back-end</span> more and become more <span>Full Stack</span>.
       </>
     ),
   },
