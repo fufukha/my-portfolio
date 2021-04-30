4548d51d8ea71d2460821a996c1d935712b5bd08
fufukha <wafakhalid29@gmail.com>
2021-04-30 16:40:03 -0400

    implemented with mui grid

--- components/BrandList.tsx
+++ components/BrandList.tsx
@@ -1,20 +1,13 @@
-import { makeStyles, Typography } from '@material-ui/core'
+import { makeStyles, Typography, Grid } from '@material-ui/core'
 import { brandData } from '../utilis/index'
 import BrandSVG from './BrandSVG'
 
 const useStyles = makeStyles({
   technology: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignSelf: 'center',
     width: '100%',
-    justifyContent: 'center',
-    marginBottom: '35px',
-    '& > div': {
-      padding: '12px',
-    },
+    margin: '0 auto 35px auto',
     '@media screen and (min-width: 960px)': {
-      justifyContent: 'space-around',
+      justifyContent: 'space-between',
       width: '90%',
     },
   },
@@ -23,8 +16,10 @@ const useStyles = makeStyles({
 const BrandList = () => {
   const classes = useStyles()
 
-  const brandSVGs = brandData.map(brand => (
-    <BrandSVG role='listitem' name={brand.name} width={brand.width} height={brand.height} color={brand.color} />
+  const brandSVGs = brandData.map((brand, i) => (
+    <Grid key={i} item>
+      <BrandSVG role='listitem' name={brand.name} width={brand.width} height={brand.height} color={brand.color} />
+    </Grid>
   ))
 
   return (
@@ -32,9 +27,9 @@ const BrandList = () => {
       <Typography component='h4' variant='srOnly'>
         Technologies
       </Typography>
-      <div role="list" className={classes.technology}>
+      <Grid container wrap='wrap' justify='center' spacing={3} role="list" className={classes.technology}>
         {brandSVGs}
-      </div>
+      </Grid>
     </section>
   )
 }
