3a9bc77f187172c5254fd516a779d6d21566f587
fufukha <wafakhalid29@gmail.com>
2021-05-12 17:14:50 -0400

    highlighttext converts heart text to icon

--- components/HighlightText.tsx
+++ components/HighlightText.tsx
@@ -1,4 +1,12 @@
+import { Typography, Icon, makeStyles } from '@material-ui/core'
 import React from 'react'
+
+const useStyles = makeStyles({
+  icon: {
+    fontSize: '1.2rem',
+    verticalAlign: 'text-bottom',
+  },
+})
 
 interface HighlightTextProps {
   children: React.PropsWithChildren<string>
@@ -10,14 +18,39 @@ const HighlightText = ({ children, words }: HighlightTextProps) => {
   const regexp = new RegExp(keywords, 'gi')
   const parts = children.split(regexp)
 
-  const newChildren = parts.map((part, i) => {
-    if (part !== undefined) {
-      if (words.includes(part.toLowerCase())) {
-        return <em key={i}>{part}</em>
+  const classes = useStyles()
+
+  const newChildren = () => {
+    let res = []
+    let prevPart = ''
+
+    for (let i = 0; i < parts.length; i++) {
+      const part = parts[i]
+
+      if (part !== undefined) {
+        if (words.includes(part.toLowerCase())) {
+          if (part === 'heart') {
+            prevPart = part
+            res.push(
+              <React.Fragment key={i}>
+                <Typography variant='srOnly'>heart</Typography>
+                <Icon className={classes.icon} fontSize='small'>
+                  favorite
+                </Icon>
+              </React.Fragment>
+            )
+          } else if (prevPart === 'heart') {
+            res.push(<React.Fragment key={i}>{part}</React.Fragment>)
+          } else {
+            res.push(<em key={i}>{part}</em>)
+          }
+        } else {
+          res.push(<React.Fragment key={i}>{part}</React.Fragment>)
+        }
       }
-      return <React.Fragment key={i}>{part}</React.Fragment>
     }
-  })
+    return res
+  }
 
   return <>{newChildren}</>
 }
