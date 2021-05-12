d9b53beda745ec31f480b2ed3369a873879bfebe
fufukha <wafakhalid29@gmail.com>
2021-05-12 17:58:25 -0400

    use repo and experience data from config.json

--- types/index.ts
+++ types/index.ts
@@ -65,38 +65,6 @@ export interface StepData {
   homepageUrl?: string
 }
 
-export enum MenuLink {
-  NAME_GITHUB = 'github',
-  NAME_LEETCODE = 'leetcode',
-  NAME_EMAIL = 'email',
-  HREF_GITHUB = 'https://github.com/fufukha',
-  HREF_LEETCODE = 'https://leetcode.com/fufukha',
-  HREF_EMAIL = 'mailto: kamile.mkb@gmail.com',
-}
-
-interface GithubLink {
-  name: MenuLink.NAME_GITHUB
-  href: MenuLink.HREF_GITHUB
-}
-
-interface LeetcodeLink {
-  name: MenuLink.NAME_LEETCODE
-  href: MenuLink.HREF_LEETCODE
-}
-
-interface EmailLink {
-  name: MenuLink.NAME_EMAIL
-  href: MenuLink.HREF_EMAIL
-}
-
-export type PersonalLinks = [GithubLink, LeetcodeLink, EmailLink]
-
-export enum Paths {
-  HOME = '/',
-  PROJECTS = '/projects',
-  JOURNEY = '/journey',
-}
-
 export type Brands =
   | 'javascript'
   | 'typescript'
